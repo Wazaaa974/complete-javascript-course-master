ce3ebf923def40faf13fae0dd67f23aa97953ddb
Thomas Alonso <thomas.alonso93@gmail.com>
2025-01-17 09:00:58 +0100

    part 2 video 17

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,26 @@
+let country = "France";
+let population = "70000000"
+let continent = "Europe"
+
+
+console.log(country)
+console.log(population)
+console.log(continent)
+
+
+const firstName = 'Jonas'
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037;
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear)
++ ' year old' + job + '!';
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}`;
+console.log(jonasNew);
+
+console.log(`Just a regular string...`);
+
+console.log(`String with \n\
+  mutliple \n\
+  lines`);
