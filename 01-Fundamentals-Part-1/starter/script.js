0513f816c7bd9fe9bf3391901a3001315c38d143
Thomas Alonso <thomas.alonso93@gmail.com>
2025-02-24 09:56:50 +0100

    JS part I 24 Logical operations

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,26 +1,59 @@
-let country = "France";
-let population = "70000000"
-let continent = "Europe"
+// let country = "France";
+// let population = "70000000"
+// let continent = "Europe"
 
 
-console.log(country)
-console.log(population)
-console.log(continent)
+// console.log(country)
+// console.log(population)
+// console.log(continent)
 
 
-const firstName = 'Jonas'
-const job = 'teacher';
-const birthYear = 1991;
-const year = 2037;
-const jonas = "I'm " + firstName + ', a ' + (year - birthYear)
-+ ' year old' + job + '!';
-console.log(jonas);
+// const firstName = 'Jonas'
+// const job = 'teacher';
+// const birthYear = 1991;
+// const year = 2037;
+// const jonas = "I'm " + firstName + ', a ' + (year - birthYear)
+// + ' year old' + job + '!';
+// console.log(jonas);
 
-const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}`;
-console.log(jonasNew);
+// const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}`;
+// console.log(jonasNew);
 
-console.log(`Just a regular string...`);
+// console.log(`Just a regular string...`);
 
-console.log(`String with \n\
-  mutliple \n\
-  lines`);
+// console.log(`String with \n\
+//   mutliple \n\
+//   lines`);
+
+//   const age = 15;
+
+//   if(age >= 18){
+// console.log('Sarah can start driving license 🏎️');
+//   } else{
+//     const yearsLeft = 18 - age;
+//     console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+//   }
+
+
+//   const birthYear = 2012;
+//   let century
+//   if(birthYear <= 2000){
+//     century = 20;
+//   } else {
+//     century = 21;
+//   }
+
+//   console.log(century);
+
+// const favourite = Number(prompt("What's your favourite number?"))
+// console.log(favourite);
+// console.log(typeof favourite);
+
+// if (favourite===23) {
+//   console.log('I love 23');
+// } else if (favourite=== 7) {
+//   console.log('7 is a cool number');
+// }
+// else  {
+//   console.log('Number is not 23 nor 7');
+// }
