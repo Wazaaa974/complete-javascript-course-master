351fe272bd962bdc62ac1816f6cd9fbf52c8676f
Thomas Alonso <thomas.alonso93@gmail.com>
2025-02-24 11:03:28 +0100

    fini partie 1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -57,3 +57,66 @@
 // else  {
 //   console.log('Number is not 23 nor 7');
 // }
+
+
+// const hasDriversLicense = true;
+// const hasGoodVision = false;
+
+// console.log(hasDriversLicense && hasGoodVision);
+// console.log(hasDriversLicense || hasGoodVision);
+
+
+// const day = 'thursday';
+
+// switch(day) {
+//   case 'monday':
+//     console.log('Plan course stucture');
+//     console.log('Go to coding meetup');
+//     break;
+//   case 'tuesday':
+//     console.log('prepare theory videos');
+//     break;
+//   case 'wednesday':
+//   case 'thursday':
+//     console.log('Write code examples');
+//     break;
+//   case 'friday':
+//     console.log('Record videos');
+//     break;
+//   case 'saturday':
+//   case 'sunday':
+//     console.log('Enjoy the weekend');
+//     break;
+//   default:
+//     console.log('Not a valid day');
+// }
+
+
+// const age =23;
+// age >= 18 ? console.log('I like to drink wine') : console.log('I like to drink water');
+
+
+
+/* Write your code below. Good luck! 🙂 */
+
+let tip = 0;
+const bill = 25;
+switch (bill) {
+    case bill >= 50 && bill <=300:
+        tip = bill*0.15;
+        console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${tip+bill}`);
+        break;
+
+    case bill < 50:
+        tip = bill*0.20;
+        console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${tip+bill}`);
+        break;
+
+    case bill > 300:
+        tip = bill*0.20;
+        console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${tip+bill}`);
+        break;
+
+    default:
+        console.log('erreur')
+}
